fd03ee123f3ad28362760a1b6e9c6aaf1930a3d8
gcreeperplayz <54014018+gcreeperplayz@users.noreply.github.com>
2022-06-16 17:52:25 -0400

    Update saturnhub.lua

--- saturnhub.lua
+++ saturnhub.lua
@@ -1,6 +1,6 @@
 game.StarterGui:SetCore("SendNotification", {
     Title = " Script Offline";
-    Text = "Someone is trying to crack the script!";
+    Text = "Saturn Hub is being updated!";
     Icon = "http://www.roblox.com/asset/?id=9666343268";
     Duration = "5";
 })
